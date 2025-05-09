3d0a5f9d261300711b7ef5db6d87552f9adcbb16
bszilard26 <baloghszilard26@gmail.com>
2025-05-10 01:50:20 +0300

    Update README and CI integration for version 1.0

--- tests/ui/test_login_ui.py
+++ tests/ui/test_login_ui.py
@@ -2,13 +2,20 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import time
 
 @pytest.mark.skipif("CI" in os.environ, reason="Skip UI test in Docker/CI")
 def test_login_swag_labs():
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com/")
+    options = Options()
+    options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get("https://www.saucedemo.com/")
     driver.find_element(By.ID, "user-name").send_keys("standard_user")
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
     driver.find_element(By.ID, "login-button").click()
