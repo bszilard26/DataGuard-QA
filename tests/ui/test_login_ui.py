7da4753ca7f47fda1b83f59025b2a2f229b262ce
bszilard26 <baloghszilard26@gmail.com>
2025-05-15 12:27:02 +0300

    Finalize POM integration, fix tests, cleanup init & gitignore

--- tests/ui/test_login_ui.py
+++ tests/ui/test_login_ui.py
@@ -1,24 +1,23 @@
 import os
 import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
 import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+from pages.login_page import LoginPage
 
 @pytest.mark.skipif("CI" in os.environ, reason="Skip UI test in Docker/CI")
 def test_login_swag_labs():
     options = Options()
     options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
 
-    driver.get("https://www.saucedemo.com/")
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
+    login_page = LoginPage(driver)
+    login_page.load()
+    login_page.login("standard_user", "secret_sauce")
 
     assert "inventory" in driver.current_url
     time.sleep(2)
